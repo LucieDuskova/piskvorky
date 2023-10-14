6fe30fd1836cb0680d10324ec2f210a7d149aa56
Lucie Dušková <lucie.duskova87@gmail.com>
2023-10-14 11:27:32 +0200

    vyhodnocení vítěze

--- hra.js
+++ hra.js
@@ -1,57 +1,71 @@
-// f-ce na změnu ikon + posluchač polí
+import { findWinner } from 'https://unpkg.com/piskvorky@0.1.4';
 
-const cell_1Element = document.querySelector("#cell_1")
-const cell_2Element = document.querySelector("#cell_2")
-const cell_3Element = document.querySelector("#cell_3")
-const cell_4Element = document.querySelector("#cell_4")
-const cell_5Element = document.querySelector("#cell_5")
-const cell_6Element = document.querySelector("#cell_6")
-const cell_7Element = document.querySelector("#cell_7")
-const cell_8Element = document.querySelector("#cell_8")
-const cell_9Element = document.querySelector("#cell_9")
-const cell_10Element = document.querySelector("#cell_10")
-let ikon_playerElement = document.querySelector("#ikon_player")
+// f-ce na změnu ikon
 
-let currentPlayer = 'circle'
+let ikon_playerElement = document.querySelector('#ikon_player');
 
+let currentPlayer = 'circle';
 
 const pridejZnak = (event) => {
-  if(currentPlayer === "circle"){
-    ikon_playerElement.src = "cross.svg"
-    event.target.classList.add("board__field--circle")
-    event.target.disabled = true
-    return currentPlayer = "cross"
-  }else if (currentPlayer === "cross"){
-    ikon_playerElement.src = "circle.svg"
-    event.target.disabled = true
-    event.target.classList.add("board__field--cross")
-    return currentPlayer = "circle"
+  if (currentPlayer === 'circle') {
+    ikon_playerElement.src = 'cross.svg';
+    event.target.classList.add('board__field--circle');
+    event.target.disabled = true;
+    currentPlayer = 'cross';
+  } else if (currentPlayer === 'cross') {
+    ikon_playerElement.src = 'circle.svg';
+    event.target.disabled = true;
+    event.target.classList.add('board__field--cross');
+    currentPlayer = 'circle';
   }
-}
+// vyber buttony z HTML
+  const noveButtons = document.querySelectorAll('.cell');
 
-cell_1Element.addEventListener("click", pridejZnak)
-cell_2Element.addEventListener("click", pridejZnak)
-cell_3Element.addEventListener("click", pridejZnak)
-cell_4Element.addEventListener("click", pridejZnak)
-cell_5Element.addEventListener("click", pridejZnak)
-cell_6Element.addEventListener("click", pridejZnak)
-cell_7Element.addEventListener("click", pridejZnak)
-cell_8Element.addEventListener("click", pridejZnak)
-cell_9Element.addEventListener("click", pridejZnak)
-cell_10Element.addEventListener("click", pridejZnak)
+ // tvorba pole
+  const buttonsArray = Array.from(noveButtons);
 
+ // tvorba pole se symbolama
+  const buttonsArraySymbol = buttonsArray.map((button2) => {
+    if (button2.classList.contains('board__field--circle')) {
+      return 'o';
+    } else if (button2.classList.contains('board__field--cross')) {
+      return 'x';
+    } else {
+      return '_';
+    }
+  });
 
+  // vyhodnocení vítěze
+  const winnerIs = findWinner(buttonsArraySymbol);
 
+  if (winnerIs === 'o') {
+    setTimeout(alert('Vyhrál hráč se symbolem kolečko!'), 300);
+  } else if (winnerIs === 'x') {
+    setTimeout(alert('Vyhrál hráč se symbolem křížek!'), 300);
+  } else if (winnerIs === 'tie') {
+    setTimeout(alert('Hra skončila remízou!'), 300);
+  }
+}; ˇ
+// až zde konče f-ce PřidejZnak - teď už i vyhodnocuje
 
+// fuknce na posluchače všech tlačítek
+
+const buttons = document.querySelectorAll('.cell');
+buttons.forEach((button) => {
+  button.addEventListener('click', pridejZnak);
+});
 
 // tlačítko restart hry - otázka
-const buttonElement = document.querySelector('#restart')
+const buttonRestartElement = document.querySelector('#restart');
 
 const restartButton = (event) => {
   const questionRestart = confirm('Opravdu chceš začít znovu?');
   if (questionRestart === false) {
-    event.preventDefault()
+    event.preventDefault();
   }
- } 
+};
 
- buttonElement.addEventListener('click', restartButton) 
+
+buttonRestartElement.addEventListener('click', restartButton);
+
+
